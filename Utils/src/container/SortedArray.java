dbf410c1b18e5909c85ce9b06786bf99d901131d
Lukas Reichmann <lukas.reichmann@kabelmail.de>
2017-01-07 17:34:11 +0100

    Very minor formatting changes
    Changed the spacing between some method's ending and next method's doc
    beginning.

--- Utils/src/container/SortedArray.java
+++ Utils/src/container/SortedArray.java
@@ -420,6 +420,7 @@ public class SortedArray<T> implements Iterable<T>
 		return elements.length;
 	}
 
+	
 	/**
 	 * Returns the amount of objects that are currently being stored in the
 	 * container.
@@ -445,6 +446,7 @@ public class SortedArray<T> implements Iterable<T>
 				Utils.isAfter(cast(elements[currentSize - 1]), element, comparable);
 	}
 
+	
 	/**
 	 * Checks if the index is smaller than 0 or bigger or equals the amount 
 	 * of the objects in the container.
@@ -512,6 +514,7 @@ public class SortedArray<T> implements Iterable<T>
 		return ret;
 	}
 
+	
 	/**
 	 * Returns the iterator that is used within a for-each loop.
 	 * 
