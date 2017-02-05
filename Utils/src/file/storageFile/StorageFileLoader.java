d8e75e68211897722e557d451721b865173a7cc8
Lukas Reichmann <lukas.reichmann@kabelmail.de>
2017-02-05 12:25:50 +0100

    Fully implemented StorageFileLoader
    Moved entire .load() functionality from StorageFile to StorageFile
    loader.

--- Utils/src/file/storageFile/StorageFileLoader.java
+++ Utils/src/file/storageFile/StorageFileLoader.java
@@ -1,6 +1,160 @@
 package file.storageFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+import exception.StorageFileException;
+
 public class StorageFileLoader
 {
+	private Scanner scanner;
 	
+	private int line = 0;
+	
+	private String currentLine;
+	
+	private ArrayList<String> commentBuffer = new ArrayList<>();
+	
+	private int lastDepth = 0;
+	private int currentDepth = 0;
+	private Entry lastChild = null;
+	private Entry lastParent;
+	
+	public Entry load(File file) throws FileNotFoundException
+	{
+		Entry ret = new Entry(null, "ROOT", "ROOT");
+		scanner = new Scanner(file);
+		
+		lastParent = ret;			//The parent of lastChild
+		
+		while(scanner.hasNextLine())
+		{
+			line++;
+			
+			currentLine = scanner.nextLine();
+			
+			if(currentLine.trim().isEmpty())
+				continue; 	//If the current line is empty, there is no need 
+							//to go on
+			
+			//Check if the line is a comment
+			if(currentLine.charAt(0) == StorageFile.COMMENT_PREFIX)
+			{
+				commentBuffer.add(currentLine.substring(1));
+				continue; //If line is a comment, there is no need to go on
+			}
+
+			//Fill currentEntry and currentDepth using regex
+			Entry currentEntry = makeEntry();
+			
+			/*
+			 * If the loop has not been continued up to this point, this line
+			 * is not a comment and the new Entry has been created 
+			 * (=> this Entry's comments have been set), so the comment buffer
+			 * will be a new instance of ArrayList to prevent the comments of
+			 * the old entry from getting changed.
+			 */
+			commentBuffer = new ArrayList<>();
+			
+			lastParent = putEntryIntoTree(currentEntry);
+			
+			lastChild = currentEntry;
+			lastDepth = currentDepth;
+		}
+		
+		scanner.close();
+		
+		return ret;
+	}
+	
+	private Entry makeEntry()
+	{
+		Matcher matcherValue = StorageFile.PATTERN_VALUE.matcher(currentLine);
+		Matcher matcherNoValue = StorageFile.PATTERN_NO_VALUE.matcher(currentLine);
+		
+		if(matcherValue.find()) //If this is a entry with a value
+		{
+			currentDepth = matcherValue.group(1).length();
+			return new Entry(commentBuffer, matcherValue.group(2), 
+					matcherValue.group(3));
+		}
+		else if(matcherNoValue.find()) //If this is a entry without a value
+		{
+			currentDepth = matcherNoValue.group(1).length();
+			return new Entry(commentBuffer, matcherNoValue.group(2), null);
+		}
+		else 	//If both matchers could not match the current line, then
+				//there is an invalid line -> error
+		{
+			scanner.close();
+			throw new StorageFileException
+								(String.format(StorageFile.MSG_INVALID_LINE, line));
+		}
+	}
+	
+	private Entry putEntryIntoTree(Entry currentEntry)
+	{
+		/*
+		 * If the current entry has the same depth as the last one (=> the 
+		 * current and last entry share the same parent).
+		 */
+		if (lastDepth == currentDepth)
+		{
+			checkKey(lastParent, currentEntry.getLocalKey());
+
+			lastParent.addChild(currentEntry);
+			
+			return lastParent;
+		}
+		
+		/*
+		 * If the current entry's depth is exactly one higher than the last one
+		 * (=> the current entry is a child of the last one).
+		 */
+		if (lastDepth == currentDepth - 1)
+		{
+			checkKey(lastChild, currentEntry.getLocalKey());
+
+			lastChild.addChild(currentEntry);
+
+			return lastChild;
+		}
+		
+		/*
+		 * If the current entry's depth is smaller of the last one 
+		 */
+		if (lastDepth > currentDepth)
+		{
+			Entry parent = lastParent;
+
+			// Decrease depth until the correct depth has been reached
+			for (int counter = lastDepth; counter > currentDepth; counter--)
+				parent = parent.getParent();
+
+			checkKey(parent, currentEntry.getLocalKey());
+
+			parent.addChild(currentEntry);
+
+			return parent;
+		}
+		
+		/*
+		 * If none of the cases above matched, currentDepth is invalid
+		 * (most likely currentDepth > (lastDepth + 1)).
+		 */
+		throw new StorageFileException(String.format(StorageFile.MSG_INVALID_DEPTH, line));
+	}
+	
+	private void checkKey(Entry entry, String key)
+	{
+		if(entry.hasChild(key))
+		{
+			scanner.close();
+			throw new StorageFileException
+					(String.format(StorageFile.MSG_KEY_ALREADY_EXISTING, key, line));
+		}
+	}
 }
